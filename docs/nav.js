89305f07aa44ccd878cc2d4622c77c4b1934619e
Efe Akbulut <efeakbulut@MacBookPro.lan>
2026-05-08 21:05:31 -0400

    Fix Dao Nguyen spelling in nav wordmark

--- docs/nav.js
+++ docs/nav.js
@@ -3,7 +3,7 @@
   nav.id = "site-nav";
   nav.innerHTML = `
     <div class="nav-inner">
-      <a class="nav-brand" href="/">Efe Akbulut&thinsp;&middot;&thinsp;Interview Follow Up with Dao Nygun</a>
+      <a class="nav-brand" href="/">Efe Akbulut&thinsp;&middot;&thinsp;Interview Follow Up with Dao Nguyen</a>
       <img class="nav-logo" src="assets/BlackLogoT.jpeg" alt="The New York Times">
     </div>`;
   document.body.prepend(nav);
